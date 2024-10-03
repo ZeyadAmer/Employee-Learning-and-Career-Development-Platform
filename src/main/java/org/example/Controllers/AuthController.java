02c7caca8626beea42eacaaf89d7f7cecd22da20
zeyad amer <zeyadamer285@gmail.com>
2024-10-03 17:27:07 +0300

    isAdmin function created

--- src/main/java/org/example/Controllers/AuthController.java
+++ src/main/java/org/example/Controllers/AuthController.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/auth")
@@ -26,5 +28,13 @@ public class AuthController {
         String token = jwtUtil.generateToken(user.getEmail(),user.getTitle().getDepartment().getName(),user.getId());
         return ResponseEntity.ok("{\"token\":\"" + token + "\"}");
     }
+    @GetMapping
+    public ResponseEntity<String> isAdmin(HttpServletRequest request) {
+        String authorizationHeader = request.getHeader("Authorization");
+        String token = authorizationHeader.substring(7);
+        String role = jwtUtil.extractRole(token);
+        return ResponseEntity.ok("{\"role\":\"" + role + "\"}");
+    }
+
 
 }
